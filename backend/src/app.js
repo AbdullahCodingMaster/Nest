8417a48a660235c9ac5b6d89aad94e3447845daa
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-07-20 13:06:45 +0500

    connecting frontent with backend by adding proxy in vite tool chain and fixiing bug related to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello, Nestian!");
+  res.json({ message: "Hello Nestian!" });
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello Nestian by backend!" });
 });
 
 export default app;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,27 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Nestian!</h1>;
-}
+// src/App.jsx
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setMessage("Error fetching data");
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend</h1>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default App;
