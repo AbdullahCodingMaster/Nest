210be7588a29c0b8be3a1c983eda26b56684cf4c
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-07-20 17:25:58 +0500

    database connection and adding some utils files and creating user models and two usefull milddlewares

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello Nestian!" });
@@ -9,4 +24,4 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello Nestian by backend!" });
 });
 
-export default app;
+export { app };
