69c376ce6ec5750b85946e2fa27f546256c3feed
AbdullahCodingMaster <MuhammadAbdullahAwan@engineer.com>
2024-07-22 09:59:50 +0500

    app updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,5 @@
-// src/App.jsx
-import { useEffect, useState } from "react";
-
 const App = () => {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    fetch("/api")
-      .then((response) => response.json())
-      .then((data) => {
-        setMessage(data.message);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setMessage("Error fetching data");
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>Frontend</h1>
-      <p>{message}</p>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 };
 
 export default App;
